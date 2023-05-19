45a96a73e3aac9d859469e30f5d09ab3fcd023c9
Armando Fandango <armando-fandango@users.noreply.github.com>
2023-05-19 11:07:21 -0700

    cleaned up old baggage to init new project

--- ezai-kernel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-conda activate ezai || source activate ezai
-# Expose environment as kernel
-python -m ipykernel install --user --name ezai --display-name "ezai"
-conda deactivate || source deactivate
